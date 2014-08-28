1a699380f2bdcc704f0139a166798221be7197ff
Thomas Ploch <thomas.ploch@meinfernbus.de>
2014-08-28 15:47:37 +0200

    Added check for security_context in ContainerEventBus

--- src/LiteCQRS/Plugin/SymfonyBundle/ContainerEventBus.php
+++ src/LiteCQRS/Plugin/SymfonyBundle/ContainerEventBus.php
@@ -11,11 +11,35 @@ class ContainerEventBus extends AbstractEventMessageBus
 {
     private $container;
     private $services;
+    private $user;
 
     public function __construct(ContainerInterface $container, array $proxyFactories = array())
     {
         $this->container = $container;
+
+        if ($this->container->has('security_context')) {
+            /** @var \Symfony\Component\Security\Core\SecurityContext $context */
+            $context = $this->container->get('security_context');
+            $token = $context->getToken();
+
+            if ($token) {
+                $this->user = $token->getUser();
+            };
+        }
+
         parent::__construct($proxyFactories);
+    }
+
+    /**
+     * @param \LiteCQRS\DomainEvent $event
+     */
+    protected function handle($event)
+    {
+        if ($this->user) {
+            $this->addUserToEvent($event);
+        }
+
+        parent::handle($event);
     }
 
     protected function getHandlers(EventName $eventName)
@@ -37,6 +61,16 @@ class ContainerEventBus extends AbstractEventMessageBus
     public function registerServices($services)
     {
         $this->services = $services;
+    }
+
+    /**
+     * @param \LiteCQRS\DomainEvent $event
+     */
+    protected function addUserToEvent($event)
+    {
+        /** @var \LiteCQRS\Bus\EventMessageHeader $header */
+        $header = $event->getMessageHeader();
+        $header->sessionId = (string) $this->user;
     }
 }
 
